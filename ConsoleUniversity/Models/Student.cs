4041e017f00b518a5ca4f7d6759833a7a31e42d0
Opilane TTHK <opilane@tthk.ee>
2024-08-26 16:40:00 +0300

    added models for student, course and enrollment

---
+++ ConsoleUniversity/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleUniversity.Models
+{
+    public class Student
+    {
+        [Key] //primary key
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
