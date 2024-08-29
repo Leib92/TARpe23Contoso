e84b29ddfad52cea4da0fabd70577bd8f1c101a3
opilane <opilane@ORDI-402599>
2024-08-29 13:15:21 +0300

    Added StudentsController and for Views the Students folder with Index

--- ConsoleUniversity/obj/Debug/net8.0/ConsoleUniversity.AssemblyInfo.cs
+++ ConsoleUniversity/obj/Debug/net8.0/ConsoleUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a33e418a1c4d72445e402be41b392deeecac65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b192636948e968f253049ae6e0280bc86f441e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
