e44d7e9fd2d8fac71af5feb00bd046a889def2db
Leib92 <opilane@HP-CZC321705Y>
2024-08-30 09:53:36 +0300

    Added migrations

--- ConsoleUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ConsoleUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84b29ddfad52cea4da0fabd70577bd8f1c101a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f6c690107f414c3aa6db3451a71c401571fcb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
