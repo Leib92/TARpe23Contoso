1d3c20072b60f57d60e7e154f0eea922ebbbf244
Leib92 <opilane@HP-CZC321705Y>
2024-09-16 13:23:37 +0300

    Added Details view and method

--- ConsoleUniversity/Controllers/StudentsController.cs
+++ ConsoleUniversity/Controllers/StudentsController.cs
@@ -19,61 +19,6 @@ namespace ConstosoUniversity.Controllers
             return View(await _context.Students.ToListAsync());
         }
 
-        /*
-        public async Task<IActionResult> Index(
-            string sortOrder,
-            string currentFilter,
-            string searchString,
-            int? pageNumber
-            )
-        {
-            ViewData["CurrentSort"] = sortOrder;
-            ViewData["NameSortparam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewData["DataSortParam"] = sortOrder == "Date" ? "date_desc" : "Date";
-
-            if (searchString != null) 
-            {
-                pageNumber = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewData["currentFilter"] = searchString;
-
-            var students = from student in _context.Students 
-                           select student;
-            if (!String.IsNullOrEmpty(searchString)) 
-            {
-                students = students.Where(student => 
-                student.LastName.Contains(searchString) || 
-                student.FirstName.Contains(searchString));
-            }
-            switch (sortOrder) 
-            {
-                case "name_desc":
-                    students = students.OrderByDescending(student => student.LastName);
-                    break;
-                case "firstname_desc":
-                    students = students.OrderByDescending(student => student.FirstName);
-                    break;
-                case "Date":
-                    students = students.OrderBy(student => student.EnrollmentDate);
-                    break;
-                case "date_desc":
-                    students = students.OrderByDescending(student => student.EnrollmentDate);
-                    break;
-                default:
-                    students = students.OrderBy(student => student.LastName);
-                    break;
-            }
-
-            int pageSize = 3;
-            return View(await _context.Students.ToListAsync());
-        }
-        */
-
         [HttpGet]
         public IActionResult Create() 
         {
@@ -119,5 +64,22 @@ namespace ConstosoUniversity.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
     }
 }
--- ConsoleUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ConsoleUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2fabe2b15623c286a83d2c56ff20292576eb6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a430edf3069e01c44d57eb42b54c896c07c6544a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
