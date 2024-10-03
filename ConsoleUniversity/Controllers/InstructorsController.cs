cd5edb5f4a2d59e9963cee7045c156823ac8f8b7
Leib92 <opilane@ORDI-402599>
2024-10-03 12:42:31 +0300

    Revert "Added Delete View to Instructors."
    This reverts commit c5ac8b242d1545a2b5019a38624a433dc2933e56.

--- ConsoleUniversity/Controllers/InstructorsController.cs
+++ ConsoleUniversity/Controllers/InstructorsController.cs
@@ -69,33 +69,5 @@ namespace ContosoUniversity.Controllers
             }
             return View(instructor);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (instructor == null)
-            {
-                return NotFound();
-            }
-
-            return View(instructor);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var instructor = await _context.Instructors.FindAsync(id);
-            _context.Instructors.Remove(instructor);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
     }
 }
